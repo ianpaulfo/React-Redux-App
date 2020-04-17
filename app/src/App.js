eaa4b2da7ed09ff7c5283dd8b3a6e6d48a4a2984
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-04-16 18:40:19 -0700

    set up reducer

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-export default App;
+
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+
+
+      </div>
+    </Provider>
+    );
+  }
---
+++ app/src/reducer/index.js
@@ -0,0 +1,4 @@
+export const reducer = (state, initialState, action) => {
+    console.log(state);
+  };
+  
